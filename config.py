df4d9e0d4b70e8a64ef8517dbd7d2469c023341a
Zoraiz Anwar <zoraizanwar.bss@gmail.com>
2026-09-06 21:01:15 +0500

    Update timetable to v1.1.4

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ Change the TIMETABLE_FILENAME here to update it across the entire application.
 """
 
 # Timetable filename - change this to update the timetable file used across the app
-TIMETABLE_FILENAME = "timetable/FSC_F26_TT_v1.1.3_29082026_DOiSXlL.xlsx"
+TIMETABLE_FILENAME = "timetable/FSC_F26_TT_v1.1.4_06092026_jp3PANC.xlsx"
 
 import os
 
